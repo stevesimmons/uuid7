82748158ab9052c7f07b6d508bc740fc9f53cfd7
Stephen Simmons <stephen@sikoia.com>
2021-12-29 00:42:07 +0000

    Separate out unit tests

--- tests/test_uuid7.py
+++ tests/test_uuid7.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
+"""
+Tests for `uuid7` package.
+"""
+import time
 
-"""Tests for `uuid7` package."""
+from uuid7 import uuid7, uuid7str
 
-import pytest
-
-
-from uuid7 import uuid7
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
+def test_uuid7():
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    Some simple tests
+    """
+    # Note the sequence value increments by 1 between each of these uuid7(...) calls
+    ns = time.time_ns()
+    out1 = str(uuid7(ns))
+    out2: str = uuid7(ns, as_type='str') # type: ignore
+    out3 = uuid7str(ns)
+
+    assert out1[:20] == out2[:20]
+    assert out2[:20] == out3[:20]
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_monotonicity():
+    last = ''
+    for n in range(100_000):
+        i = uuid7str()
+        if n > 0 and i <= last:
+            raise RuntimeError(f"UUIDs are not monotonic: {last} versus {i}")
